76b6cd61b6f143b3b2c277a1ea91c971869984bd
SocioPJ <20.00704-3@maua.br>
2022-11-05 19:42:22 -0300

    teste gameplay

--- Projeto 5/Assets/Scripts/Guns/ShotgunShootingScript.cs
+++ Projeto 5/Assets/Scripts/Guns/ShotgunShootingScript.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class ShotgunShootingScript : MonoBehaviour
 {
     public Transform firePoint;
-    public Transform firePoint2;
-    public Transform firePoint3;
+    private Transform firePoint2;
+    private Transform firePoint3;
     public GameObject bulletPrefab;
     public float bulletForce = 20f;
 
@@ -21,13 +22,17 @@ public class ShotgunShootingScript : MonoBehaviour
 
     void Shoot()
     {
+        firePoint2 = firePoint;
+        firePoint3 = firePoint;
+        firePoint2.rotation*= Quaternion.Euler(0,0,5);
+        firePoint3.rotation*= quaternion.Euler(0,0,-5);
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
-        GameObject bullet2 = Instantiate(bulletPrefab, firePoint2.position, firePoint2.rotation);
+        GameObject bullet2 = Instantiate(bulletPrefab, firePoint.position, firePoint2.rotation);
         Rigidbody2D rb2 = bullet2.GetComponent<Rigidbody2D>();
         rb2.AddForce(firePoint2.up * bulletForce, ForceMode2D.Impulse);
-        GameObject bullet3 = Instantiate(bulletPrefab, firePoint3.position, firePoint3.rotation);
+        GameObject bullet3 = Instantiate(bulletPrefab, firePoint.position, firePoint3.rotation);
         Rigidbody2D rb3 = bullet3.GetComponent<Rigidbody2D>();
         rb3.AddForce(firePoint3.up * bulletForce, ForceMode2D.Impulse);
     }
