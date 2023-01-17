44bafb57e63eb5e11dcdfdd2a679117f1d9b03af
SocioPJ <20.00704-3@maua.br>
2023-01-17 20:24:57 -0300

    created ChangeCameraPosition script

---
+++ Projeto 5/Assets/Scripts/Camera/ChangeCameraPosition.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCameraPosition : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log(Camera.main.transform.position);
+        }
+    }
+}
