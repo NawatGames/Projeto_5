d9239e30184c6a2b66ac58c403a7fa748fb48fa4
Sakaef03 <fosakae@icloud.com>
2023-01-19 00:12:16 -0300

    continuação upgrades

--- Projeto 5/Assets/Scripts/WeaponUpgrader.cs
+++ Projeto 5/Assets/Scripts/WeaponUpgrader.cs
@@ -13,34 +13,39 @@ public class WeaponUpgrader : MonoBehaviour
         StateNameController.sncfirepoint = 1;
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "FireRateUpgrade")
         {
             StateNameController.sncfirerate++;
+            Debug.Log("Firerate");
         }
 
         if(collision.gameObject.tag == "DamageUpgrade")
         {
             StateNameController.sncdamage++;
+            Debug.Log("DMG");
         }
 
-        if(collision.gameObject.tag == "FirePoint") //&& StateNameController.sncfirepoint < 2)
+        if(collision.gameObject.tag == "FirePoint" && StateNameController.sncfirepoint < 2)
         {
             StateNameController.sncfirepoint++;
+            Debug.Log("Firepoint");
         }
 
-        //if(collision.gameObject.tag == "FirePoint" && StateNameController.sncfirepoint > 1)
-        //{
-        //    randomizer = Random.Range(1,2);
-        //    if(randomizer == 1)
-        //    {
-        //          StateNameController.sncdamage++
-        //    }
-        //    if(randomizer == 2)
-        //    {
-        //          StateNameController.sncfirerate++
-        //    }
-        //}
+        if(collision.gameObject.tag == "FirePoint" && StateNameController.sncfirepoint >= 2)
+        {
+            randomizer = Random.Range(1,2);
+            if(randomizer == 1)
+            {
+                  StateNameController.sncdamage++;
+                  Debug.Log("DMG");
+            }
+            if(randomizer == 2)
+            {
+                  StateNameController.sncfirerate++;
+                  Debug.Log("Firerate");
+            }
+        }
     }
 }
