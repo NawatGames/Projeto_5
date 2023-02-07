a6f7f9efe9b30bcfcb39032680905f9ffe9566a4
SocioPJ <20.00704-3@maua.br>
2023-02-06 21:09:13 -0300

    td certo

--- Projeto 5/Assets/Scripts/Maps/RoomTemplates.cs
+++ Projeto 5/Assets/Scripts/Maps/RoomTemplates.cs
@@ -14,26 +14,26 @@ public class RoomTemplates : MonoBehaviour
     public GameObject closedRoom;
     public float waitTime;
     private bool spawnedBoss;
-    //public GameObject boss;
+    public GameObject boss;
 
-    // private void Update()
-    // {
-    //     if (waitTime <= 0 && spawnedBoss == false)
-    //     {
-    //         for (int i = 0; i < rooms.Count; i++)
-    //         {
-    //             if (i == rooms.Count - 1)
-    //             {
-    //                 Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
-    //                 spawnedBoss = true;
-    //             }
-    //         }
-    //     }
-    //     else
-    //     {
-    //         waitTime -= Time.deltaTime;
-    //     }
-    // }
+    private void Update()
+    {
+        if (waitTime <= 0 && spawnedBoss == false)
+        {
+            for (int i = 0; i < rooms.Count; i++)
+            {
+                if (i == rooms.Count - 1)
+                {
+                    Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
+                    spawnedBoss = true;
+                }
+            }
+        }
+        else
+        {
+            waitTime -= Time.deltaTime;
+        }
+    }
 }
     
 
